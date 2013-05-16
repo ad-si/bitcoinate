4779c019c4f1c098d47938dc7e1a556456c0b165
adius <mail@adriansieber.com>
2013-05-16 09:07:58 +0200

    Improved execution order

--- src/bitcoinate.js
+++ src/bitcoinate.js
@@ -1,26 +1,27 @@
 //bitcoinate {{ VERSION }} by Adrian Sieber (adriansieber.com)
 
-!function (window, document) {
+!function(window, document) {
 
-	var style = document.createElement('style'),
-		buttons = document.getElementsByClassName('bitcoinate'),
-		sentence = 'Please donate bitcoins to: ',
-		i,
-		data
+	var style = document.createElement('style')
 
 	style.type = 'text/css'
 	style.innerHTML = '{{ CSS }}'
 	document.getElementsByTagName('head')[0].appendChild(style)
 
-	document.addEventListener('DOMContentLoaded', function(){
+	document.addEventListener('DOMContentLoaded', function() {
 
-		for (i = 0; i < buttons.length; i++) {
+		var buttons = document.getElementsByClassName('bitcoinate'),
+			sentence = 'Please donate bitcoins to: ',
+			data,
+			i
+
+		for(i = 0; i < buttons.length; i++) {
 
 			buttons[i].title = sentence + buttons[i].dataset.address
 
 			buttons[i].innerHTML = '<span></span>bitcoinate'
 
-			buttons[i].addEventListener('click', function () {
+			buttons[i].addEventListener('click', function() {
 				data = this.dataset
 
 				if(data.type == 'URI')
