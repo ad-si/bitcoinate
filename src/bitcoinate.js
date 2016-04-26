3be6a7f2059dab948534151892fc9ad4437615b4
Adrian Sieber <mail@adriansieber.com>
2016-04-26 11:21:21 +0000

    Use 2 spaces instead of tabs

--- src/bitcoinate.js
+++ src/bitcoinate.js
@@ -2,38 +2,38 @@
 
 !function(window, document) {
 
-	var style = document.createElement('style')
+  var style = document.createElement('style')
 
-	style.type = 'text/css'
-	style.innerHTML = '{{ CSS }}'
-	document.getElementsByTagName('head')[0].appendChild(style)
+  style.type = 'text/css'
+  style.innerHTML = '{{ CSS }}'
+  document.getElementsByTagName('head')[0].appendChild(style)
 
-	document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
 
-		var buttons = document.getElementsByClassName('bitcoinate'),
-			sentence = 'Please donate bitcoins to: ',
-			data,
-			i
+    var buttons = document.getElementsByClassName('bitcoinate'),
+      sentence = 'Please donate bitcoins to: ',
+      data,
+      i
 
-		for(i = 0; i < buttons.length; i++) {
+    for(i = 0; i < buttons.length; i++) {
 
-			buttons[i].title = sentence + buttons[i].dataset.address
+      buttons[i].title = sentence + buttons[i].dataset.address
 
-			buttons[i].innerHTML = '<span></span>bitcoinate'
+      buttons[i].innerHTML = '<span></span>bitcoinate'
 
-			buttons[i].addEventListener('click', function() {
-				data = this.dataset
+      buttons[i].addEventListener('click', function() {
+        data = this.dataset
 
-				if(data.type == 'URI')
-					window.location.href = 'bitcoin:' + data.address
-						+ '?amount=' + data.address
-						+ '&label=' + data.label
-				else
-					window.prompt(sentence, data.address)
+        if(data.type == 'URI')
+          window.location.href = 'bitcoin:' + data.address
+            + '?amount=' + data.address
+            + '&label=' + data.label
+        else
+          window.prompt(sentence, data.address)
 
-			}, false)
-		}
+      }, false)
+    }
 
-	}, false)
+  }, false)
 
 }(window, document)
